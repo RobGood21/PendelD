b793d8d84fcbc40019feaf0dad73982e1ee5bcbc
Rob Antonisse <robantonisse@gmail.com>
2020-02-09 16:45:36 +0100

    PendelDCC

--- PendelDCC.ino
+++ PendelDCC.ino
@@ -18,8 +18,8 @@ byte count_preample;
 byte count_byte;
 byte count_bit;
 byte count_command;
+int count_slow;
 
-unsigned int count_slow;
 byte dcc_fase;
 byte dcc_data[5]; //bevat te verzenden DCC bytes, current DCC commando
 byte dcc_aantalBytes; //aantal bytes current van het DCC commando
@@ -28,8 +28,15 @@ byte sw_status; //laatste stand van switches
 //eventeel nog een buffer om meerdere DCC commandoos te hebben bij bv. 2 locs of accessoire sturing
 byte dcc_buf1[5];
 byte dcc_buf2[5];
-byte dcc_buf3[5];
-byte dcc_bufreg[5];
+byte dcc_buf3[5]; //max 3 bytes 
+byte dcc_bufCount[5];//how many times command must be send
+byte dcc_bufAantalBytes[5];
+byte dcc_bufActive; // bit 0= buffer 0 // bit4 = buffer 4
+
+
+byte loc_adress = 6; //moet van EEPROM komen
+byte loc_speed;
+byte loc_function=B10000000; //moet van EEPROM komen
 
 
 //temps
@@ -37,18 +44,16 @@ volatile unsigned long time;
 volatile unsigned long countpuls;
 unsigned long time_slow;
 byte functies;
+byte newlocadres=10;
 
 
 void setup() {
 	Serial.begin(9600);
-
 	//poorten
 	DDRB |= (1 << 3); //set PIN11 as output for DCC 
 	PORTC |= (15 << 0); //set pin A0, A1 pull up
 	DDRB |= (1 << 0); //PIN8 as output enable DCC
-	
-
-
+	   
 	sw_status = 0xFF;
 
 	//interrupt register settings
@@ -63,13 +68,10 @@ void setup() {
 	//OCR2A – Output Compare Register A tijdsduur 0~255
 	OCR2A = 115; //geeft een puls van 58us
 	TIMSK2 |= (1 << 1);
-
-
-	
-
+	   	 
 	PORTB |= (1 << 0); //set pin8 high
 	functies = B10000000;
-	DCC_command();
+	//DCC_command();
 
 }
 
@@ -86,7 +88,7 @@ ISR(TIMER2_COMPA_vect) {
 
 		case 1: //preample zenden
 			count_preample++;
-			if (count_preample > 13) {
+			if (count_preample > 22) {
 				count_preample = 0;
 				dcc_fase = 2;
 				FalseBit;
@@ -108,12 +110,12 @@ ISR(TIMER2_COMPA_vect) {
 			else { //count_bit 8 or more
 				count_bit = 7;
 				if (count_byte < dcc_aantalBytes) {
-					count_byte ++; //next byte
+					count_byte++; //next byte
 					FalseBit;
 				}
 				else { //command send reset	
 
-					dcc_fase = 0; 
+					dcc_fase = 0;
 					TrueBit;
 				}
 			}
@@ -127,26 +129,86 @@ ISR(TIMER2_COMPA_vect) {
 	}
 }
 
-
-void DCC_command_nieuw() {
+void PRG_locadres() { //set new locadres
+	/*
+	zo gaat ut niet zie de CV instellingen moeten exclusief minimaal 2x worden verstuurd, 
+	Beste een 'aparte' buffer nemen voor de CV bv. buffer[2] en een flag zetten in een register als programmode... 
+	dan alleen de cv versturen...
 	
+	
+	
+	*/
 
 
-	count_command++;
-	//if(count_command > 4)count_command=0)
+
+
+	//1110CCVV 0 VVVVVVVV 0 DDDDDDDD
+
+	//find free buffer
+	for (byte i = 2; i < 5; i ++) { 
+		if(bitRead(dcc_bufActive,i)==false){
+		//if (dcc_bufActive & ~(1 << i)) {
+
+			Serial.println(dcc_bufActive);
+
+			dcc_bufActive |= (1 << i); //claim buffer
+
+			Serial.println(dcc_bufActive);
+
+			dcc_buf1[i] = B11101100;
+			dcc_buf2[i] = B00000000; //CV1
+			dcc_buf3[i] = B00001010; // newlocadres;
+			dcc_bufCount[i] = 4;
+			dcc_bufAantalBytes[i] = 3;
+			i = 10;
+		}
+	}
 }
 
+void DCC_command() { //nieuwe
+	count_command++;
+	if (count_command > 4)count_command = 0;
+	switch (count_command) {
+	case 0: //speed and direction
+		dcc_data[0] = loc_adress;
+		dcc_data[1] = loc_speed;
+		dcc_aantalBytes = 2;
+		break;
+	case 1: //Functions
+		dcc_data[0] = loc_adress;
+		dcc_data[1] = loc_function;
+		dcc_aantalBytes = 2;
+		break;
 
-
-void DCC_command() {
-	dcc_aantalBytes = 3;
-	//dcc_data
-	dcc_data[0] = B00000110; //adres 6
-	//dcc_data[0] = B00001110; //adres 14
-	//dcc_data[0] = B00011100; //adres 28
-	dcc_data[1] = B01110001; // functies;
-	dcc_data[2] = dcc_data[0] ^ dcc_data[1];
-
+	default: //case others (accessoires or so)
+		if (dcc_bufActive & (1 << count_command) ) {
+			if (dcc_bufCount[count_command] == 0) { //disable command
+				dcc_bufActive &=~(1 << count_command); 
+			}
+			else { //make command				
+				dcc_data[0] = dcc_buf1[count_command];
+				dcc_data[1] = dcc_buf2[count_command];
+				dcc_data[2] = dcc_buf3[count_command];
+				dcc_aantalBytes = dcc_bufAantalBytes[count_command];
+				dcc_bufCount[count_command]--;
+			}			
+		}
+		else { //Send idle command
+			
+			dcc_data[0] = 0xFF;
+			dcc_data[1] = 0x00;
+			dcc_aantalBytes = 2;
+		}
+		break;
+	}
+	switch (dcc_aantalBytes) {
+	case 2:
+		dcc_data[2] = dcc_data[0] ^ dcc_data[1];
+		break;
+	case 3:
+		dcc_data[3] = dcc_data[0] ^ dcc_data[1] ^ dcc_data[2];
+		break;
+	}
 }
 
 void SW_exe() {
@@ -161,53 +223,36 @@ void SW_exe() {
 	sw_status = poort;
 
 	//DCC
-	//DCC_command();
+	DCC_command();
 	dcc_fase = 1;
 	count_preample = 0; //niet nodig?
 
 }
-void SW_on_F(byte sw) { //functions
-		dcc_data[1] ^= (1 << sw+1);
-
-	dcc_data[2] = dcc_data[0] ^ dcc_data[1];
-	Serial.println(dcc_data[1], BIN);
-}
-
-void SW_on(byte sw) {  //drive
-	//Serial.println(sw);
-
+void SW_on(byte sw) { //nieuw
 	switch (sw) {
 	case 0:
-		PINB |= (1 << 0);
+		PINB |= (1 << 0); //no DCC
 		break;
 	case 1:
-		///GPIOR0 ^= (1 << 1);
-		//if (GPIOR0 & (1 << 1)) {/
-			dcc_data[1] = B01111111;
-			dcc_data[2] = dcc_data[0] ^ dcc_data[1];		
+		loc_speed = B01101110; //drive
 		break;
 	case 2:
-		dcc_data[1] = B01110001;
-		dcc_data[2] = dcc_data[0] ^ dcc_data[1];
+		loc_speed = B01100000; //stop
 		break;
 	case 3:
+		PRG_locadres();
+		//loc_function ^= (1 << 4); //headlights
 		break;
 	}
-	Serial.println(dcc_data[1], BIN);
 
 }
-void loop() {
-/*
 
-	if (millis() - time_slow > 20) {
-		time_slow = millis();
-		SW_exe();
-	}
-*/
-	
-	
+void loop() {
+
+	//slow events timer
 	count_slow++;
-	if (count_slow == 0) {
+	if (count_slow > 10000) { //makes frequency of command sending important 5000 is too fast
+		count_slow = 0;
 		//slow events
 		SW_exe();
 	}
